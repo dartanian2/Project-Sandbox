08102387c20dd0bb3048c4a76d91077d5f4543df
dartanian2 <45007749+dartanian2@users.noreply.github.com>
2018-12-29 13:43:19 +0000

    General Edits

--- DepthGetterSetter/Scripts/TerrainGenerator.cs
+++ DepthGetterSetter/Scripts/TerrainGenerator.cs
@@ -1,29 +1,42 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
-    
+
     //private float divisor = 3725f;
-    private const float divisor = 1000f;
+    /// <summary>
+    /// Max value depth should be, is equivalent to 0 because inversion, is used to scale
+    /// </summary>
+    private const float MaxDistance = 2000f;
+
+    /// <summary>
+    /// Min value depth should be, is equivalent to 1f because inversion
+    /// </summary>
+    private const float MinDistance = 500f;
+
+    /// <summary>
+    /// Max value terrain mesh can be
+    /// </summary>
     private const int depth = 100;
+    
     //3725f is max value that you want to measure
     //official max is 4500f but I dont think so
 
 
     public void UpdateTerrain(float[,] depthMap, int width, int height)
     {
-        //updates as every third kinect frame comes in
         Terrain terrain = GetComponent<Terrain>();
         TerrainData tD = terrain.terrainData;
         tD.heightmapResolution = width + 1;
         tD.size = new Vector3(width, depth, height);
-        Debug.Log("Actual Value: "+depthMap[211, 255]);
+
+        Debug.Log("Actual Value: " + depthMap[211, 255]);
 
         //depthMap = Divide(depthMap, width, height);
         depthMap = Clean(depthMap, width, height);
-        Debug.Log("Multiplier: "+depthMap[211, 255]);
+        Debug.Log("Multiplier: " + depthMap[211, 255]);
         tD.SetHeights(0, 0, depthMap);
         //Debug.Log(depthMap[212, 212]);
     }
@@ -54,38 +67,50 @@ public class TerrainGenerator : MonoBehaviour
     private float[,] Divide(float[,] depthMap, int width, int height)
     {
         //should height and width be switched around? I think so but it breaks when that is done?! And no error is returned
+        //dont know what this is on about tbh ^^
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                depthMap[x, y] = depthMap[x, y] / divisor;
+                depthMap[x, y] = depthMap[x, y] / MaxDistance;
             }
         }
         return depthMap;
     }
 
+    //get float value, minus 500f, divide by 2000f, scale and round to 2sf, invert
+    //biggest depths should be smallest and visa versa
+    //if depth is zero, it should be ignored, so copy neighbour?
+
+    //I also don't think my compare4x ect actually work
+
     private float[,] Clean(float[,] depthMap, int width, int height)
     {
-        for (int x=0; x < width; x++)
+        for (int x = 0; x < width; x++)
         {
-            for (int y=0; y < height; y++)
+            for (int y = 0; y < height; y++)
             {
-                if (depthMap[x, y] > divisor || depthMap[x, y] == 0)
+                if (depthMap[x, y] > MaxDistance || depthMap[x, y] == 0)
                 {
+                    //erroneous, ignore or copy adjacent
                     depthMap[x, y] = 0f;
-                    depthMap = Compare4x(depthMap, x, y);
+                    //depthMap = Compare4x(depthMap, x, y);
                     //depthMap = Compare9x(depthMap, x, y);
                 }
-                else if (depthMap[x, y] < 500f && depthMap[x, y] != 0)
+                else if (depthMap[x, y] < MinDistance && depthMap[x, y] != 0)
                 {
+                    //erroneous, ignore or copy adjacent
                     depthMap[x, y] = 1f;
-                    depthMap = Compare4x(depthMap, x, y);
+                    //depthMap = Compare4x(depthMap, x, y);
                     //depthMap = Compare9x(depthMap, x, y);
                 }
                 else
                 {
-                    depthMap[x, y] = (divisor - (depthMap[x, y] - 500f)) / divisor;
-                    depthMap = Compare4x(depthMap, x, y);
+                    //move value closer to the camera, then divide, then round
+                    float preRounding = (depthMap[x, y] - MinDistance) / MaxDistance;
+                    //round float to 2 sf, then invert
+                    depthMap[x, y] = 1f - (Mathf.Round(preRounding * 100f) / 100f);
+                    //depthMap = Compare4x(depthMap, x, y);
                     //depthMap = Compare9x(depthMap, x, y);
                 }
             }
@@ -97,10 +122,19 @@ public class TerrainGenerator : MonoBehaviour
     //Has to point up left so that we are not comparing to values that have not been processed yet (Any value right or down of pointer has not been processed yet)
 
     //Should try to assign lower weight to values further away from pointer, make new function to do this
-    //can we do all of this with a for loop? Yes lol
 
     //Use terrainData.GetSteepness to get rid of stupidly steep values??
-    
+
+
+
+
+    //Shouldn't all (heightIndex - 1) be (heightIndex + 1) ??
+
+
+
+
+
+    //Smaller box blur filter
     private float[,] Compare4x(float[,] depthMap, int widthIndex, int heightIndex)
     {
         if (widthIndex > 0 && heightIndex > 0)
@@ -131,6 +165,7 @@ public class TerrainGenerator : MonoBehaviour
     }
 
     //Does not work, why is this???
+    //Box blur filter
     private float[,] Compare9x(float[,] depthMap, int widthIndex, int heightIndex)
     {
         if (widthIndex > 1 && heightIndex > 1)
